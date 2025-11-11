d1716c2020396dbf67c0980426dfa7d2843e772b
a pineapple <90796271+suspicious-pineapple@users.noreply.github.com>
2025-11-11 14:20:19 +0100

    look harder for the xsdp

--- kernel/src/system.c
+++ kernel/src/system.c
@@ -9,20 +9,21 @@ void init_system(){
     
     print_string(&kglobals.console, "\r\nParsing XSDP..\r\nOEM ID:");
     char oem_str[7];
-    memcpy(oem_str, system.xsdp->oem_id,6);
-    oem_str[6]=0;
-    
-    print_string(&kglobals.console, oem_str);
     print_string(&kglobals.console, "\r\nACPI ver.:");
     print_hex_byte(&kglobals.console, system.xsdp->revision);
     print_string(&kglobals.console, "\r\n");
     
-
+    memcpy(oem_str, system.xsdp->oem_id,6);
+    oem_str[6]=0;
+    
+    print_string(&kglobals.console, oem_str);
+    print_string(&kglobals.console, "\r\n");
+    
     uint8_t checksum;
     for(uint16_t i = 0; i < sizeof(struct XSDP);i++){
         checksum = *(uint8_t*)(i+(uint64_t)system.xsdp);
 
-    print_hex64(&kglobals.console, i);
+    print_hex64(&kglobals.console, checksum);
  
     print_string(&kglobals.console, "\r\n");
 
